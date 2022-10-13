346333c90ec47a5a8a7e54c11afe6b7f100b702f
ren95 <71228030+ren95@users.noreply.github.com>
2022-10-13 17:19:05 +0200

    -

--- App.tsx
+++ App.tsx
@@ -1,8 +1,30 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View, SafeAreaView,  } from "react-native";
+import { Text, View, SafeAreaView } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
+import { useState, useEffect } from "react";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  function parseTasks(data: string) {
+    JSON.parse("tasks", data);
+  }
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const data = await fetch("https://hyperbolic.vercel.app/api/tasks")
+        .then((response) => response.json())
+        .then((response) => parseTasks(response))
+        .then((response) => setTasks(response));
+
+      console.log(data);
+    };
+
+    fetchTasks().catch(console.error);
+  }, []);
+
   return (
     <TailwindProvider>
       <View className="flex-1 items-center justify-center bg-black ">
